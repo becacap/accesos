8c502bafca91a9a57d3734d1246c2ec2cbddd907
profe <becacapsept2020@gmail.com>
2020-09-25 09:43:21 +0200

    arreglo test

--- src/test/java/cap/curso/accesos/EstadosTest.java
+++ src/test/java/cap/curso/accesos/EstadosTest.java
@@ -1,16 +1,12 @@
 package cap.curso.accesos;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import cap.curso.jpa.configuracion.Configuracion;
-import cap.curso.jpa.entidades.Estado;
-import cap.curso.jpa.estado.exception.EstadoNotFoundException;
-import cap.curso.jpa.estado.servicios.EstadosServiceInterface;
+import cap.curso.accesos.entidades.Estado;
+import cap.curso.accesos.estado.exception.EstadoNotFoundException;
+import cap.curso.accesos.estado.servicios.EstadosServiceInterface;
 
 @SpringBootTest
 public class EstadosTest
--- src/test/java/cap/curso/accesos/TestFichajesR.java
+++ src/test/java/cap/curso/accesos/TestFichajesR.java
@@ -1,19 +1,12 @@
 package cap.curso.accesos;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import cap.curso.jpa.configuracion.Configuracion;
-import cap.curso.jpa.entidades.Usuario_Estado;
-import cap.curso.jpa.services.UsuariosEstadoServiceInterface;
+import cap.curso.accesos.services.UsuariosEstadoServiceInterface;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Configuracion.class)
+@SpringBootTest
 public class TestFichajesR
 {
 
