c231540773abff65a6b93136264655113175231a
Carlos <airedits97@gmail.com>
2020-09-25 10:55:03 +0200

    terminado método buscar jornada por id

--- src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
+++ src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,10 +64,9 @@ public class RestAccesosController
 	}
 	
 	@GetMapping("/jornadaId/{id}")
-	public Optional<Jornada> getJornada(@RequestParam("id") int id)
+	public Optional<Jornada> getJornada(@PathVariable int id)
 	{
-		Optional<Jornada> jornada = getJornadaServiceInterface().findById(id);
-		return jornada;
+		return getJornadaServiceInterface().findById(id);
 	}
 	
 	
