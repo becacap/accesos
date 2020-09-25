6e905a3d149c8f4870d51ea46e955a91042c42bf
Jorge T. <becacapsept2020@gmail.com>
2020-09-25 08:49:47 +0200

    Modificar Jornada

--- src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
+++ src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
@@ -1,18 +1,17 @@
 package cap.curso.accesos.controladores;
 
 
-import java.util.List;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import cap.curso.accesos.entidades.Empleado;
 import cap.curso.accesos.entidades.Estado;
+import cap.curso.accesos.entidades.Jornada;
 import cap.curso.accesos.estado.servicios.EstadosServiceInterface;
 
 
@@ -24,11 +23,31 @@ public class RestAccesosController
 	@Autowired
 	private EstadosServiceInterface estadosServiceInterface;
 	
+	@Autowired
+	private cap.curso.accesos.servicios.JPAJornadaServiceInterface jpaJornadaSI;
+	
+	@Autowired
+	private cap.curso.accesos.servicios.JPAEmpleadoServiceInterface jpaEmpleadoSI;
+	
 	@GetMapping("/ejemplo")
 	public Estado home(@RequestParam("descripcion") String descripcion) {
 		return getEstadosServiceInterface().findByDescripcion(descripcion);
 	}
-
+	
+	@PostMapping("/modificarJornada")
+	public java.util.Optional<Empleado> modificarJornada(@RequestBody("empleado") java.util.Optional<Empleado> empleado, Jornada jornada) {
+		Jornada jornadaNueva = new Jornada();
+		jornadaNueva.setLunes("9.00-20.00");
+		jornadaNueva.setMartes("9.00-20.00");
+		jornadaNueva.setMiercoles("9.00-20.00");
+		jornadaNueva.setJueves("9.00-20.00");
+		jornadaNueva.setViernes("9.00-20.00");
+		jornadaNueva.setDescripcion("Jornada completa");
+		jpaJornadaSI.save(jornadaNueva);
+		
+		empleado.get().setJornada(jornadaNueva);
+		return empleado;
+	}
 	public EstadosServiceInterface getEstadosServiceInterface()
 	{
 		return estadosServiceInterface;
--- src/test/java/cap/curso/accesos/EstadosTest.java
+++ src/test/java/cap/curso/accesos/EstadosTest.java
@@ -3,6 +3,7 @@ package cap.curso.accesos;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,8 +12,7 @@ import cap.curso.jpa.entidades.Estado;
 import cap.curso.jpa.estado.exception.EstadoNotFoundException;
 import cap.curso.jpa.estado.servicios.EstadosServiceInterface;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = Configuracion.class)
+@SpringBootTest
 public class EstadosTest
 {
 
