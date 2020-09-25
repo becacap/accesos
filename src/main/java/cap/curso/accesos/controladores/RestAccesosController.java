2a75635fc78dc55d9fa381849e7b540d8f7fd3d0
Carlos <airedits97@gmail.com>
2020-09-25 12:19:29 +0200

    cambiada logica metodo insertar jornada

--- src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
+++ src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
@@ -19,6 +19,7 @@ import cap.curso.accesos.entidades.Empleado;
 import cap.curso.accesos.entidades.Estado;
 import cap.curso.accesos.entidades.Jornada;
 import cap.curso.accesos.estado.servicios.EstadosServiceInterface;
+import cap.curso.accesos.servicios.JPAEmpleadoServiceInterface;
 import cap.curso.accesos.servicios.JPAJornadaServiceInterface;
 
 @RestController
@@ -28,12 +29,11 @@ public class RestAccesosController
 	@Autowired
 	private EstadosServiceInterface estadosServiceInterface;
 
-
 	@Autowired
 	private JPAJornadaServiceInterface jPAJornadaServiceInterface;
 
 	@Autowired
-	private cap.curso.accesos.servicios.JPAEmpleadoServiceInterface jpaEmpleadoSI;
+	private JPAEmpleadoServiceInterface jpaEmpleadoSI;
 
 	@GetMapping("/ejemplo")
 	public Estado home(@RequestParam("descripcion") String descripcion)
@@ -42,17 +42,27 @@ public class RestAccesosController
 	}
 	
 	@GetMapping("/add-jornada")
-	public Jornada introducirJornada() {
-		Jornada jornada = new Jornada();
+	public Jornada introducirJornada(@RequestBody Jornada jornada) {
+		
+		//comprobar si el id es 0 o no. SI es 0 se inserta, si no se descarta
+		
+		/*Jornada jornada = new Jornada();
 		jornada.setDescripcion("Jornada Completa");
 		jornada.setLunes("8:00-14:00&15:00-17:30");
 		jornada.setMartes("8:00-14:00&15:00-17:30");
 		jornada.setMiercoles("8:00-14:00&15:00-17:30");
 		jornada.setJueves("8:00-14:00&15:00-17:30");
 		jornada.setViernes("8:00-15:00");
-		jornada.setEspecial(0);
+		jornada.setEspecial(0);*/
 		
-		return getJornadaServiceInterface().save(jornada);
+		if(jornada.getId() == 0)
+		{
+			return getJornadaServiceInterface().save(jornada);
+		}
+		else
+		{
+			return null;
+		}
 	}
 	
 	@GetMapping("/find-all-jornadas")
