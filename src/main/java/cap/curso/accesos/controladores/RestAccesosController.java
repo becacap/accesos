ebcf8d760256b992e984854bff39711662f4e664
Sergio <sergio.de-oro-gutierrez@capgemini.com>
2020-09-25 09:05:01 +0200

    carga rama jornada

--- src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
+++ src/main/java/cap/curso/accesos/controladores/RestAccesosController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import cap.curso.accesos.entidades.Estado;
+import cap.curso.accesos.entidades.Jornada;
 import cap.curso.accesos.estado.servicios.EstadosServiceInterface;
+import cap.curso.accesos.servicios.JPAJornadaServiceInterface;
 
 
 
@@ -24,9 +26,38 @@ public class RestAccesosController
 	@Autowired
 	private EstadosServiceInterface estadosServiceInterface;
 	
+	@Autowired
+	private JPAJornadaServiceInterface jornadaServiceInterface;
+	
 	@GetMapping("/ejemplo")
 	public Estado home(@RequestParam("descripcion") String descripcion) {
 		return getEstadosServiceInterface().findByDescripcion(descripcion);
+	}
+	
+	@GetMapping("jornada")
+	public Jornada introducirJornada() {
+		Jornada jornada = new Jornada();
+		jornada.setDescripcion("Jornada Completa");
+		jornada.setLunes("8:00-14:00&15:00-17:30");
+		jornada.setMartes("8:00-14:00&15:00-17:30");
+		jornada.setMiercoles("8:00-14:00&15:00-17:30");
+		jornada.setJueves("8:00-14:00&15:00-17:30");
+		jornada.setViernes("8:00-15:00");
+		jornada.setEspecial(0);
+		
+		return getJornadaServiceInterface().save(jornada);
+	}
+	
+	
+
+	public JPAJornadaServiceInterface getJornadaServiceInterface()
+	{
+		return jornadaServiceInterface;
+	}
+
+	public void setJornadaServiceInterface(JPAJornadaServiceInterface jornadaServiceInterface)
+	{
+		this.jornadaServiceInterface = jornadaServiceInterface;
 	}
 
 	public EstadosServiceInterface getEstadosServiceInterface()
