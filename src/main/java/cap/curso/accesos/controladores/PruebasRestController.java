801038d87e52b5d7f1d73723645ff236aa861966
Eider <eider.irazoqui-goncalves@capgemini.com>
2020-10-06 12:17:06 +0200

    crossOrigin added

--- src/main/java/cap/curso/accesos/controladores/PruebasRestController.java
+++ src/main/java/cap/curso/accesos/controladores/PruebasRestController.java
@@ -5,18 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cap.curso.accesos.entidades.Estado;
 import cap.curso.accesos.servicios.EstadosServiceInterface;
 
 @RestController
+@RequestMapping("/api")
 public class PruebasRestController
 {
 	@Autowired
 	private EstadosServiceInterface estadosServiceInterface;
-	@CrossOrigin(origins = "http://localhost:4200")
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@GetMapping("/estados")
 	public List<Estado> getEstados(){
 		
